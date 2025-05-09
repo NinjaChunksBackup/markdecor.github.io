d27ae58950c8a95ab157dfbde001bed6764a87f4
MadamjiBirthday <plexanywhere3@gmail.com>
2025-05-09 19:07:11 +0530

    fifth

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-from flask import Flask, request, render_template, redirect, url_for
-import json
+from flask import Flask, render_template
+
 app = Flask(__name__)
-# Load page mappings from a configuration file
 
-with open('pages.json', 'r') as f:
-
-    page_map = json.load(f)
 @app.route('/')
-
 def index():
     return render_template('index.html')
-@app.route('/<page>')
 
-def load_page(page):
-    if page in page_map:
-        return render_template(page_map[page])
-    else:
-        return redirect(url_for('index'))
-app = app
+@app.route('/brochure')
+def brochure_page():
+    return render_template('brochure.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
